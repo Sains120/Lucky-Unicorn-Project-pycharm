333de295332435a47a8a02357b39c42398cae3c6
Sains120 <sainsburys@middleton.school.nz>
2023-03-31 15:08:40 +1300

    changes

---
+++ 06_rounds_v1.py
@@ -0,0 +1,4 @@
+"""Component 4 - game mechanics and looping v1
+Based on 05_token_generator_v4 but har-wired to only allocate donkeys
+Gives user feedback about number of rounds and maintains balance.
+test amount set to $5"""
